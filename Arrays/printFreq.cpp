9de439ae8702e25c53fba8bfbbf58125f64cbf24
IshitaSharma3101 <72185572+IshitaSharma3101@users.noreply.github.com>
2021-08-26 23:24:53 +0530

    printFreq

---
+++ Arrays/printFreq.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+void printFreq(int arr[], int n){
+    int freq=1, i=1;
+    while(i<n){
+        while(i<n && arr[i]==arr[i-1]){
+            freq++;
+            i++;
+        }
+        cout<<arr[i-1]<<" "<<freq<<endl;
+        i++;
+        freq=1;
+    }
+    if(n==1 || arr[n-1]!=arr[n-2]){
+        cout<<arr[n-1]<<" "<<1;
+    }
+}
+
+int main(){
+    int arr[]={10,10,10,20,20,30};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    printFreq(arr,n);
+}
