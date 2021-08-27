3a52d17f8cfdd7f16b8d117bd9bc82b789881325
IshitaSharma3101 <72185572+IshitaSharma3101@users.noreply.github.com>
2021-08-27 11:33:48 +0530

    trappingRainWater

---
+++ Arrays/trappingRainWater.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int trappingRainWater(int arr[], int n){
+    int lmax[n],rmax[n],res=0;
+    lmax[0]=arr[0];
+    for(int i=1;i<n;i++){
+        lmax[i]=max(arr[i],lmax[i-1]);
+    }
+    rmax[n-1]=arr[n-1];
+    for(int i=n-2;i>=0;i--){
+        rmax[i]=max(arr[i],rmax[i+1]);
+    }
+    for(int i=1;i<n-1;i++){
+        res += min(lmax[i],rmax[i])-arr[i];
+    }
+    return res;
+}
+
+int main(){
+    int arr[]={5,0,5,2,3};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    cout<<trappingRainWater(arr,n);
+}
